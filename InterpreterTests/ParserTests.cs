0b250b937da0e32801d04c0c091cf78e7f586d39
LeonSteinbach <leon@steinba.de>
2023-02-19 21:42:00 +0100

    Added test class for parser

---
+++ InterpreterTests/ParserTests.cs
@@ -0,0 +1,29 @@
+﻿namespace InterpreterTests
+{
+	[TestClass]
+	public class ParserTests
+	{
+		[TestMethod]
+		public void TestLetStatement()
+		{
+			string input = "let x = 0;" +
+			               "let y = 1;";
+			Lexer lexer = new Lexer(input);
+			Parser parser = new Parser(lexer);
+
+			Program program = parser.ParseProgram();
+
+			Assert.IsTrue(program.Statements.Count == 2);
+
+			Assert.IsTrue(((LetStatement)program.Statements[0]).Token.Type == TokenType.LET);
+			Assert.IsTrue(((LetStatement)program.Statements[0]).Token.Literal == "let");
+			Assert.IsTrue(((LetStatement)program.Statements[0]).Name.Value == "x");
+			Assert.IsTrue(((LetStatement)program.Statements[0]).Value == null);
+
+			Assert.IsTrue(((LetStatement)program.Statements[1]).Token.Type == TokenType.LET);
+			Assert.IsTrue(((LetStatement)program.Statements[1]).Token.Literal == "let");
+			Assert.IsTrue(((LetStatement)program.Statements[1]).Name.Value == "y");
+			Assert.IsTrue(((LetStatement)program.Statements[1]).Value == null);
+		}
+	}
+}
