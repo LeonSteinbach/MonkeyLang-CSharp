d197ac89cd864eada94f3ba92fa5fd8ad666fd0d
LeonSteinbach <leon@steinba.de>
2023-02-19 21:40:39 +0100

    Added first ast nodes

---
+++ Interpreter/Ast.cs
@@ -0,0 +1,65 @@
+﻿namespace Interpreter
+{
+	public interface Node
+	{
+		public string TokenLiteral();
+	}
+
+	public interface Statement : Node
+	{
+		public Node? StatementNode();
+	}
+
+	public interface Expression : Node
+	{
+		public Node? ExpressionNode();
+	}
+
+	public class Program : Node
+	{
+		public List<Statement> Statements { get; set; }
+
+		public Program()
+		{
+			Statements = new List<Statement>();
+		}
+
+		public string TokenLiteral()
+		{
+			return Statements.Count > 0 ? Statements[0].TokenLiteral() : string.Empty;
+		}
+	}
+
+	public class Identifier : Expression
+	{
+		public Token Token { get; set; }
+		public string Value { get; set; }
+
+		public Node? ExpressionNode()
+		{
+			return null;
+		}
+
+		public string TokenLiteral()
+		{
+			return Token.Literal;
+		}
+	}
+
+	public class LetStatement : Statement
+	{
+		public Token Token { get; set; }
+		public Identifier Name { get; set; }
+		public Expression Value { get; set; }
+
+		public Node? StatementNode()
+		{
+			return null;
+		}
+
+		public string TokenLiteral()
+		{
+			return Token.Literal;
+		}
+	}
+}
